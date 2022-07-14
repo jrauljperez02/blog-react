85ad1521935a054a6018d2ed54991670545a0e46
jrauljperez02 <raul_jimenez_71@outlook.es>
2022-07-14 00:01:28 -0500

    Create skills section

--- src/components/Content/Skills/Bar.jsx
+++ src/components/Content/Skills/Bar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import './skills.css'
 
 const Bar = ({props}) => {
   return (
     <div className='bar'>
         <div className="info">
             <span>{props.name}</span>
-            <span>{props.level}%</span>
+            <span>{props.level}</span>
         </div>
-        <div className = {props.style}></div>
+        <div class= {`line ${props.name}`}></div>
     </div>
   )
 }
--- src/components/Content/Skills/Skills.jsx
+++ src/components/Content/Skills/Skills.jsx
@@ -4,7 +4,35 @@ import {
     Link,
   } from "react-router-dom";
 
+// import Bar from './Bar';
+import './skills.css'
+
+
 const Skills = () => {
+
+//     const data_bar = [
+//     {
+//         name: 'HTML/CSS/JavaScript',
+//         level :'90%'
+//     },
+//     {
+//         name: 'REACTJS',
+//         level: '90%'
+//     },
+//     {
+//         name: 'Python - Django/ Django Rest Framework',
+//         level: '90%'
+//     },
+//     {
+//         name: 'Docker',
+//         level: '90%'
+//     },
+//     {
+//         name: 'English',
+//         level: 'C1'
+//     }
+// ]
+
   return (
     <section className='skills'>
         <div className='title'>
@@ -21,11 +49,49 @@ const Skills = () => {
                         bastannte fuertes para aprender posteriormente, Python y sus respectivos modulos y frameworks. Y posteriormente,
                         aprender lenguajes de marcado como HTML5, CSS3, JavaScript
                     </p>
-                    <Link to = 'projects'>Look my projects</Link>
+                    <Link className = 'btn' to = 'projects'>Look my projects</Link>
                 </div>
             </Router>
-
-
+       
+        <div className="column col-right reveal">
+            <div className="bar">
+            <div className="bar">
+            <div className="info">
+              <span>HTML/CSS/JAVASCRIPT</span>
+              <span>95%</span>
+            </div>
+            <div className="line html"></div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>REACTJS</span>
+              <span>95%</span>
+            </div>
+            <div className="line reactjs"></div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>PYTHON DJANGO/DJANGO REST FRAMEWORK</span>
+              <span>80%</span>
+            </div>
+            <div className="line django"></div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>MACHINE LEARNING PYTHON</span>
+              <span>30%</span>
+            </div>
+            <div className="line machinelearning"></div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>DOCKER</span>
+              <span>90%</span>
+            </div>
+            <div className="line docker"></div>
+          </div>
+            </div>
+        </div>
         </div>
     </section>
   )
